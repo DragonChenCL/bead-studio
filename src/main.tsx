a4da91ba5de1ef3c0ffeb4ab63b7840705b8471d
DragonChenCL <chenlong_qrr@163.com>
2026-09-07 14:05:06 +0800

    style: load unified studio experience layer

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import './palette-theme.css';
 import './experience.css';
 import './h5.css';
 import './brand.css';
+import './studio-v07.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
